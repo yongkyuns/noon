4a1c96ddd3099ea73454326481602962c2270182
Yongkyun Shin <yongkyuns@gmail.com>
2026-08-24 17:57:54 -0400

    Merge latest runtime reactive modules

--- crates/noon-runtime/src/reactive.rs
+++ crates/noon-runtime/src/reactive.rs
@@ -3,8 +3,14 @@ include!("reactive_impl.rs");
 mod host_callbacks;
 pub use host_callbacks::*;
 
+mod host_policy;
+pub use host_policy::*;
+
 mod native_inputs;
 pub use native_inputs::*;
 
 mod signal_timeline;
 pub use signal_timeline::*;
+
+mod timeline_scheduler;
+pub use timeline_scheduler::*;
