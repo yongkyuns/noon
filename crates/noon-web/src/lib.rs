dd989afd76d157235819d886c14ac449d23a22d7
Yongkyun Shin <yongkyuns@gmail.com>
2026-08-24 04:48:47 -0400

    Export animation option bridge

--- crates/noon-web/src/lib.rs
+++ crates/noon-web/src/lib.rs
@@ -1,8 +1,10 @@
 #![forbid(unsafe_code)]
 
+mod authoring_options;
 #[path = "legacy.rs"]
 mod legacy;
 mod reactive_player;
 
+pub use authoring_options::*;
 pub use legacy::*;
 pub use reactive_player::*;
