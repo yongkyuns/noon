64eef392d2e7215fd92f3caca686478a93194e60
Yongkyun Shin <yongkyuns@gmail.com>
2026-08-24 05:31:22 -0400

    Export composition bridge

--- crates/noon-web/src/lib.rs
+++ crates/noon-web/src/lib.rs
@@ -1,12 +1,14 @@
 #![forbid(unsafe_code)]
 
 mod authoring_options;
+mod composition;
 #[path = "legacy.rs"]
 mod legacy;
 mod lifecycle;
 mod reactive_player;
 
 pub use authoring_options::*;
+pub use composition::*;
 pub use legacy::*;
 pub use lifecycle::*;
 pub use reactive_player::*;
