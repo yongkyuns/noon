635baf042da0f5fb4ef1a40d0d6d69e2a77b650c
Yongkyun Shin <yongkyuns@gmail.com>
2026-08-26 12:54:05 -0400

    Add exact-source Manim LaggedStartMapExample

---
+++ web/python/examples/manim_example_lagged_start_map.py
@@ -0,0 +1,20 @@
+from noon import *
+
+
+class LaggedStartMapExample(Scene):
+    def construct(self):
+        title = Tex("LaggedStartMap").to_edge(UP, buff=LARGE_BUFF)
+        dots = VGroup(
+            *[Dot(radius=0.16) for _ in range(35)]
+            ).arrange_in_grid(rows=5, cols=7, buff=MED_LARGE_BUFF)
+        self.add(dots, title)
+
+        # Animate yellow ripple effect
+        for mob in dots, title:
+            self.play(LaggedStartMap(
+                ApplyMethod, mob,
+                lambda m : (m.set_color, YELLOW),
+                lag_ratio = 0.1,
+                rate_func = there_and_back,
+                run_time = 2
+            ))
