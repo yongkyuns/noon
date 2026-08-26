2a4a52a83e9933cf25bf327668cb16bcbcbe405e
Yongkyun Shin <yongkyuns@gmail.com>
2026-08-26 13:51:35 -0400

    Add Restore parity fixtures

--- parity/manim-v0.21/quickstart.py
+++ parity/manim-v0.21/quickstart.py
@@ -589,3 +589,14 @@ class SpinInFromNothingRectangle(Scene):
             stroke_opacity=0.0,
         ).rotate(PI / 7).shift(1.5 * RIGHT + 0.5 * UP)
         self.play(SpinInFromNothing(rectangle))
+
+
+class RestoreExample(Scene):
+    def construct(self):
+        s = Square()
+        s.save_state()
+        self.play(FadeIn(s))
+        self.play(s.animate.set_color(PURPLE).set_opacity(0.5).shift(2*LEFT).scale(3))
+        self.play(s.animate.shift(5*DOWN).rotate(PI/4))
+        self.wait()
+        self.play(Restore(s), run_time=2)
---
+++ parity/manim-v0.21/upstream-examples/restore_example.py
@@ -0,0 +1,12 @@
+from manim import *
+
+
+class RestoreExample(Scene):
+    def construct(self):
+        s = Square()
+        s.save_state()
+        self.play(FadeIn(s))
+        self.play(s.animate.set_color(PURPLE).set_opacity(0.5).shift(2*LEFT).scale(3))
+        self.play(s.animate.shift(5*DOWN).rotate(PI/4))
+        self.wait()
+        self.play(Restore(s), run_time=2)
