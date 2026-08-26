6867071a8cfab02f6d98d05242363624854cc41e
Yongkyun Shin <yongkyuns@gmail.com>
2026-08-26 12:54:16 -0400

    Add upstream Manim SpinInFromNothingExample source

---
+++ parity/manim-v0.21/upstream-examples/spin_in_from_nothing_example.py
@@ -0,0 +1,10 @@
+from manim import *
+
+
+class SpinInFromNothingExample(Scene):
+    def construct(self):
+        squares = [Square() for _ in range(3)]
+        VGroup(*squares).set_x(0).arrange(buff=2)
+        self.play(SpinInFromNothing(squares[0]))
+        self.play(SpinInFromNothing(squares[1], angle=2 * PI))
+        self.play(SpinInFromNothing(squares[2], point_color=RED))
