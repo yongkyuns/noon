ff8f3bfce9a76aae050bc0687b98c55e07e92bcd
Yongkyun Shin <yongkyuns@gmail.com>
2026-08-26 12:48:24 -0400

    Add exact-source Manim EllipseExample

---
+++ web/python/examples/manim_example_ellipse.py
@@ -0,0 +1,9 @@
+from noon import *
+
+
+class EllipseExample(Scene):
+    def construct(self):
+        ellipse_1 = Ellipse(width=2.0, height=4.0, color=BLUE_B)
+        ellipse_2 = Ellipse(width=4.0, height=1.0, color=BLUE_D)
+        ellipse_group = Group(ellipse_1,ellipse_2).arrange(buff=1)
+        self.add(ellipse_group)
