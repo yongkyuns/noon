a5cd0cd68224cb582c4762fcee090aac80c44ae2
Yongkyun Shin <yongkyuns@gmail.com>
2026-08-26 19:16:25 -0400

    Add clean Manim MoveToTarget parity

--- parity/manim-v0.21/quickstart.py
+++ parity/manim-v0.21/quickstart.py
@@ -578,6 +578,18 @@ class IndicateSquare(Scene):
         self.play(Indicate(square))
 
 
+class MoveToTargetExample(Scene):
+    def construct(self):
+        c = Circle()
+
+        c.generate_target()
+        c.target.set_fill(color=GREEN, opacity=0.5)
+        c.target.shift(2*RIGHT + UP).scale(0.5)
+
+        self.add(c)
+        self.play(MoveToTarget(c))
+
+
 class SpinInFromNothingRectangle(Scene):
     def construct(self):
         rectangle = Rectangle(
---
+++ web/python/examples/manim_example_move_to_target.py
@@ -0,0 +1,12 @@
+from noon import *
+
+class MoveToTargetExample(Scene):
+    def construct(self):
+        c = Circle()
+
+        c.generate_target()
+        c.target.set_fill(color=GREEN, opacity=0.5)
+        c.target.shift(2*RIGHT + UP).scale(0.5)
+
+        self.add(c)
+        self.play(MoveToTarget(c))
