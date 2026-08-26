71ab7270a56ee9d3a1d1379a3e558dff617b960e
Yongkyun Shin <yongkyuns@gmail.com>
2026-08-26 12:51:48 -0400

    Add exact-source Manim GrowFromEdgeExample

---
+++ web/python/examples/manim_example_grow_from_edge.py
@@ -0,0 +1,11 @@
+from noon import *
+
+
+class GrowFromEdgeExample(Scene):
+    def construct(self):
+        squares = [Square() for _ in range(4)]
+        VGroup(*squares).set_x(0).arrange(buff=1)
+        self.play(GrowFromEdge(squares[0], DOWN))
+        self.play(GrowFromEdge(squares[1], RIGHT))
+        self.play(GrowFromEdge(squares[2], UR))
+        self.play(GrowFromEdge(squares[3], UP, point_color=RED))
