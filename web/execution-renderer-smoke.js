82db7c8e7cec3257a8fdae56fd7bc8abf5c2201f
Yongkyun Shin <yongkyuns@gmail.com>
2026-09-05 21:45:47 -0400

    test: render visible execution fallback fixture

--- web/execution-renderer-smoke.js
+++ web/execution-renderer-smoke.js
@@ -2,6 +2,7 @@ import init, {
   AuthoringSceneCore,
   EngineScenePlayer,
   ExecutionCanvasRenderer,
+  authoringCircle,
 } from "./pkg/noon_web.js";
 
 const state = {
@@ -15,6 +16,8 @@ window.noonExecutionRendererSmoke = state;
 async function start() {
   await init();
   const bootstrap = new AuthoringSceneCore();
+  const circle = bootstrap.add(authoringCircle(0.75));
+  bootstrap.moveTo(circle, 0, 0);
   const engine = new EngineScenePlayer(bootstrap.sceneJson(), 4.0, 1);
   const initialDelta = engine.initialDeltaJson();
   const canvas = new OffscreenCanvas(960, 540);
