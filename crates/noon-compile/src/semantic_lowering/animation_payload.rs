88627eb7d40e3e2acdde9666d78d3a047aa0b81a
Yongkyun Shin <yongkyuns@gmail.com>
2026-09-11 08:26:26 +0900

    Export family Transform activation preparation

--- crates/noon-compile/src/semantic_lowering/animation_payload.rs
+++ crates/noon-compile/src/semantic_lowering/animation_payload.rs
@@ -1,5 +1,6 @@
 mod affine;
 mod family_transform;
+mod family_transform_activation;
 mod prepared_composition;
 mod scheduled_captures;
 mod text_write;
@@ -7,6 +8,7 @@ mod transform_payload;
 
 pub use affine::*;
 pub use family_transform::*;
+pub use family_transform_activation::*;
 pub use prepared_composition::*;
 pub use text_write::*;
 pub use transform_payload::*;
