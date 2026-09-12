f946d4801a9fa5c594a7d5cea81da61e1ec6bc24
Yongkyun Shin <yongkyuns@gmail.com>
2026-09-12 12:03:55 +0900

    Make omitted-range parity fixture sparse

--- parity/manim-v0.21/core-examples/arrow_vector_field.py
+++ parity/manim-v0.21/core-examples/arrow_vector_field.py
@@ -1,10 +1,22 @@
 from manim import *
 
 
+def sparse_default_field(point):
+    x = point[0]
+    y = point[1]
+    if y not in (-3.0, 0.0, 3.0):
+        return ORIGIN
+    if x == -6.0:
+        return 0.25 * RIGHT
+    if x == 0.0:
+        return 1.0 * UP
+    if x == 6.0:
+        return 2.0 * LEFT
+    return ORIGIN
+
+
 class ArrowVectorFieldStatic(Scene):
     def construct(self):
-        field = ArrowVectorField(
-            lambda point: point[0] * UP - point[1] * RIGHT,
-        )
+        field = ArrowVectorField(sparse_default_field)
         self.add(field)
         self.wait(0.2)
