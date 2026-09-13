b01837a6db47891eaedfb029e1b8896f0013e350
Yongkyun Shin <yongkyuns@gmail.com>
2026-09-14 03:28:34 +0900

    Add source-equivalent SVG Transform parity fixture

--- parity/manim-v0.21/core-examples/svg_static_import.py
+++ parity/manim-v0.21/core-examples/svg_static_import.py
@@ -11,6 +11,14 @@ SVG_SOURCE = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 60">
   </g>
 </svg>"""
 
+SVG_MORPH_SOURCE = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+  <path d="M10 50 L50 10 L90 50 L50 90 Z" fill="#58C4DD"/>
+</svg>"""
+
+SVG_MORPH_TARGET = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+  <path d="M15 25 L85 25 L85 75 L15 75 Z" fill="#58C4DD"/>
+</svg>"""
+
 
 class SvgStaticImport(Scene):
     def construct(self):
@@ -19,3 +27,16 @@ class SvgStaticImport(Scene):
         icon = SVGMobject(str(path))
         self.add(icon)
         self.wait(0.2)
+
+
+class SvgTransform(Scene):
+    def construct(self):
+        source_path = Path("/tmp/noon_svg_morph_source.svg")
+        target_path = Path("/tmp/noon_svg_morph_target.svg")
+        source_path.write_text(SVG_MORPH_SOURCE)
+        target_path.write_text(SVG_MORPH_TARGET)
+
+        source = SVGMobject(str(source_path))
+        target = SVGMobject(str(target_path))
+        self.add(source)
+        self.play(Transform(source, target), run_time=1.0, rate_func=linear)
